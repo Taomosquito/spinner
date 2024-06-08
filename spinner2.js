fe7857c21b88044696af6265fe6166c1e168d3b8
Taomosquito <S_Peloquin123@live.com>
2024-06-07 22:01:52 -0400

    add second version of spinner that uses setInterval to keep it spinning indefinitely making it more reusable

---
+++ spinner2.js
@@ -0,0 +1,12 @@
+setInterval(() => {
+  process.stdout.write('\r|   ');
+}, 500);
+setInterval(() => {
+  process.stdout.write('\r-   ');
+}, 625);
+setInterval(() => {
+  process.stdout.write('\r/   ');
+}, 750);
+setInterval(() => {
+  process.stdout.write('\r-   ');
+}, 875);
